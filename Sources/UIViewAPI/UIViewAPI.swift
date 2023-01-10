4ce419a0374438db29cf04ca21f035ba0b1856bc
Dominika Kokowicz <50965392+dominika-kokowicz@users.noreply.github.com>
2023-01-10 19:07:23 +0100

    Update UIViewAPI.swift
    Added extension for UILabel + 3 methods for width and numberOfLines

--- Sources/UIViewAPI/UIViewAPI.swift
+++ Sources/UIViewAPI/UIViewAPI.swift
@@ -20,4 +20,24 @@ public extension UIView {
     }
 }
 
+public extension UILabel {
+    var getIntrinsicLabelWidth: CGFloat {
+        get { self.intrinsicContentSize.width }
+    }
+    
+    var getLabelWidth: CGFloat {
+        get { self.frame.width }
+    }
+    
+    func numberOfLines() -> Int {
+      guard let myText = self.text as NSString? else {
+        return 0
+      }
+      // Call self.layoutIfNeeded() if your view uses auto layout
+      let rect = CGSize(width: self.bounds.width, height: CGFloat.greatestFiniteMagnitude)
+      let labelSize = myText.boundingRect(with: rect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: self.font as Any], context: nil)
+      return Int(ceil(CGFloat(labelSize.height) / self.font.lineHeight))
+    }
+}
+
 #endif
