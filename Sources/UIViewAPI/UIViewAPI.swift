beb5c32d3c04fa57fb981ffcc19a2773fbeb00c6
Dominika Kokowicz <dominika.kokowicz@gmail.com>
2023-01-18 23:04:55 +0100

    Added doc in code

--- Sources/UIViewAPI/UIViewAPI.swift
+++ Sources/UIViewAPI/UIViewAPI.swift
@@ -21,19 +21,21 @@ public extension UIView {
 }
 
 public extension UILabel {
+    /// Get intrinsic width of UILabel (width of a label based on its content - font and text)
     var intrinsicWidth: CGFloat {
         get { self.intrinsicContentSize.width }
     }
     
+    /// Get width of UILabel
     var width: CGFloat {
         get { self.frame.width }
     }
     
+    /// Get lines count of UILabel, call self.layoutIfNeeded() if your view uses auto layout
     var linesCount: Int {
       guard let myText = self.text as NSString? else {
         return 0
       }
-      // Call self.layoutIfNeeded() if your view uses auto layout
       let rect = CGSize(width: self.bounds.width, height: CGFloat.greatestFiniteMagnitude)
       let labelSize = myText.boundingRect(with: rect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: self.font as Any], context: nil)
       return Int(ceil(CGFloat(labelSize.height) / self.font.lineHeight))
